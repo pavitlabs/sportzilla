3dfe895cd6e8112ed41766f3cdcf37d087709d81
Sai Hegde <saihegde@gmail.com>
2014-04-17 08:57:50 -0600

    Adding Mongo dependency and employee routes

--- app.js
+++ app.js
@@ -6,8 +6,10 @@
 var express = require('express');
 var routes = require('./routes');
 var user = require('./routes/user');
+var employees = require('./routes/employee');
 var http = require('http');
 var path = require('path');
+
 
 var app = express();
 
@@ -32,6 +34,10 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+
+app.get('/employees/:id/reports', employees.findByManager);
+app.get('/employees/:id', employees.findById);
+app.get('/employees', employees.findAll);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
