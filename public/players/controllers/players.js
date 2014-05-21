828318e002e4d6afcc041d3b1f8d8a4dc8cb7818
Sai Hegde <SaiHegde@gmail.com>
2014-05-21 06:25:16 -0600

    CRUD Players.

---
+++ public/players/controllers/players.js
@@ -0,0 +1,66 @@
+'use strict';
+
+angular.module('mean.teams').controller('TeamsController', ['$scope', '$stateParams', '$location', 'Global', 'Teams', function ($scope, $stateParams, $location, Global, Teams) {
+    $scope.global = Global;
+
+    $scope.create = function() {
+        var team = new Teams({
+            name: this.name,
+            parentTeamId: this.parentTeamId,
+            city: this.city,
+            state: this.state,
+            country: this.country
+        });
+        team.$save(function(response) {
+            $location.path('teams/' + response._id);
+        });
+
+        this.name = '';
+        this.parentTeamId = '';
+        this.city = '';
+        this.state = '';
+        this.country = '';
+    };
+
+    $scope.remove = function(team) {
+        if (team) {
+            team.$remove();
+
+            for (var i in $scope.teams) {
+                if ($scope.teams[i] === team) {
+                    $scope.teams.splice(i, 1);
+                }
+            }
+        }
+        else {
+            $scope.team.$remove();
+            $location.path('teams');
+        }
+    };
+
+    $scope.update = function() {
+        var team = $scope.team;
+        if (!team.updated) {
+            team.updated = [];
+        }
+        team.updated.push(new Date().getTime());
+
+        team.$update(function() {
+            $location.path('teams/' + team._id);
+        });
+    };
+
+    $scope.find = function() {
+        Teams.query(function(teams) {
+            $scope.teams = teams;
+        });
+    };
+
+    $scope.findOne = function() {
+        Teams.get({
+            teamId: $stateParams.teamId
+        }, function(team) {
+            $scope.team = team;
+        });
+    };
+}]);
